c10726a3e41c3e47201531f84c130209917094e1
Christophe Baron <2054172@cstj.qc.ca>
2020-09-03 14:55:42 -0500

    C'est le premier commit/validation du projet Labo01

---
+++ Projet/TEST.cpp
@@ -0,0 +1,4 @@
+// But: Ecrire un programme qui affiche un message a l'ecran et apprendre a gerer les versions du code avec l'application Git et de le publier sur GitHub
+// Auteur: Christophe Baron
+// Date: 2020-09-03
+
